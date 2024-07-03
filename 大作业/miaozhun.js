355fa4c36cd27273965b59b34824bcc3ec98b8b0
去码头整的SHUTIAO <1508850051@qq.com>
2024-07-03 14:21:29 +0800

    Update miaozhun.js

--- 大作业/miaozhun.js
+++ 大作业/miaozhun.js
@@ -21,8 +21,7 @@ function startTraining() {
     document.getElementById('targets-hit').innerText = targetsHit;
     // 调用spawnTarget函数生成游戏目标
     spawnTarget();
-    // 使用setTimeout函数设置一个定时器timer，在游戏持续时间结束后调用endTraining函数。
-    同时使用setInterval函数设置一个定时器countdown，每隔1秒调用updateTimer函数更新倒计时。
+    // 使用setTimeout函数设置一个定时器timer，在游戏持续时间结束后调用endTraining函数。同时使用setInterval函数设置一个定时器countdown，每隔1秒调用updateTimer函数更新倒计时。
     timer = setTimeout(endTraining, gameDuration * 1000);
     countdown = setInterval(updateTimer, 1000);
     // 记录游戏开始的时间
@@ -32,16 +31,14 @@ function startTraining() {
 
 // spawnTarget函数。作用是生成游戏目标。
 function spawnTarget() {
-    // 获取id为'game-area'的元素，然后创建一个新的div元素作为游戏目标。
-    设置该目标元素的类名为'target'，并随机设置其在游戏区域内的位置
+    // 获取id为'game-area'的元素，然后创建一个新的div元素作为游戏目标.设置该目标元素的类名为'target'，并随机设置其在游戏区域内的位置
     const gameArea = document.getElementById('game-area');
     const target = document.createElement('div');
     target.className = 'target';
     target.style.top = `${Math.random() * 90}%`;
     target.style.left = `${Math.random() * 90}%`;
     gameArea.appendChild(target);
-// 将目标元素添加到游戏区域中。为目标元素添加一个点击事件监听器，当点击目标时，击中目标次数targetsHit加1，
-    更新显示击中目标次数的元素的文本内容，移除被点击的目标元素，然后再次调用spawnTarget函数生成新的目标
+// 将目标元素添加到游戏区域中。为目标元素添加一个点击事件监听器，当点击目标时，击中目标次数targetsHit加1.更新显示击中目标次数的元素的文本内容，移除被点击的目标元素，然后再次调用spawnTarget函数生成新的目标
     target.addEventListener('click', () => {
         targetsHit++;
         document.getElementById('targets-hit').innerText = targetsHit;
